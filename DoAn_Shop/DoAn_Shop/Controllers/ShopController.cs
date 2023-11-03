d6d715cdc880f0886eae38bd559df97317b0e36b
phanhop123 <115680048+phanhop123@users.noreply.github.com>
2023-11-03 16:17:55 +0700

    C:/Program Files/Git/update

---
+++ DoAn_Shop/DoAn_Shop/Controllers/ShopController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn_Shop.Controllers
+{
+	public class ShopController : Controller
+	{
+		// GET: ShopController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: ShopController/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: ShopController/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: ShopController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: ShopController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: ShopController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: ShopController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: ShopController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
